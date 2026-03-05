96282207ee0380417f1acb583d6bec1b7ca9578f
ruwei779-pixel <ruwei779@gmail.com>
2026-03-05 12:19:08 +0000

    1

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask,render_template,request
 
-app = Flask(__name__)
+app=Flask(__name__)
 
 @app.route("/",methods=["get","post"])
 def index():
     return(render_template("index.html"))
 
-
 @app.route("/main",methods=["GET","POST"])
 def main():
-    q = request.form.get("q")
-    print(q)
     return(render_template("main.html"))
 
-if __name__ == "__main__":
+@app.route("/ethics",methods=["GET","POST"])
+def ethics():
+    return(render_template("ethics.html"))
+
+@app.route("/correct",methods=["GET","POST"])
+def correct():
+    return(render_template("correct.html"))
+
+@app.route("/wrong",methods=["GET","POST"])
+def wrong():
+    return(render_template("correct.html"))
+
+if __name__=="__main__":
     app.run()
-
-
